dbf0531c4a5f6eee8f9d8b366a50ff48354325cb
kacper.polak <kacper.polak@kalambagames.com>
2021-03-25 14:26:45 +0100

    LAB-3 adding a method to close the visit every hour

---
+++ src/main/java/uj/pwkp/gr1/vet/VetApp/other/ScheduledTask.java
@@ -0,0 +1,29 @@
+package uj.pwkp.gr1.vet.VetApp.other;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import uj.pwkp.gr1.vet.VetApp.entity.Status;
+import uj.pwkp.gr1.vet.VetApp.entity.Visit;
+import uj.pwkp.gr1.vet.VetApp.service.VisitService;
+
+@Component
+public class ScheduledTask {
+
+  @Autowired
+  VisitService visitService;
+
+  @Scheduled(fixedRate = 3600)
+  public void updateVisitStatusEveryHour() {
+    var visitList = visitService.getAllVisits();
+    visitList.forEach(v -> {
+      if (ChronoUnit.MINUTES.between(v.getStartTime(), LocalDateTime.now()) < 0) {
+        visitService.updateVisitStatus(v.getId(), Status.FINISHED_AUTOMATICALLY);
+      }
+    });
+  }
+
+}
